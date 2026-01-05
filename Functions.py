3f6b03991bbe062989f4cc33ee7f805011d58bd6
bartekwaskii <bartek20030121@gmail.com>
2026-01-06 00:35:10 +0100

    Simpified CheckMessage function

--- Functions.py
+++ Functions.py
@@ -1,14 +1,13 @@
 import numpy
 
 def CheckMessage(_request): # funkcja sprawdzająca poprawność typu danych w zapytaniu http post
-    if _request.is_json:
-        receivedData = _request.get_json()
-        if 'Heights' in receivedData:
-            return True
-        else:
-            return 'ERROR: no value named "Heights"', 400
+    if not _request.is_json:
+        return 'ERROR: request is not JSON', 400   
+    receivedData = _request.get_json()
+    if 'Heights' in receivedData:
+        return True
     else:
-        return 'ERROR: request is not JSON', 400
+            return 'ERROR: no value named "Heights"', 400
     
 def StartSettingHeights(_heights: numpy.ndarray):
     # tutaj będzie kod odpowiedzialny z sterowanie stdzienkami
