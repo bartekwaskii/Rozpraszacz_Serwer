69fd31d45853f742c90b0b126bee8bc34408ff14
bartekwaskii <bartek20030121@gmail.com>
2026-03-13 10:23:42 +0100

    test

---
+++ serwerCopy.py
@@ -0,0 +1,71 @@
+# serwer 1 dla, studzienek 1-15
+
+from flask import Flask, request, jsonify
+import numpy
+import Functions
+
+app = Flask(__name__)
+
+class HeightsClass:
+
+    def __init__(self):
+        self.__heights = numpy.zeros(15) # zmienna z wysokościami do ustawienia/aktualnymi po ustawieniu
+        self.__currentPositions = numpy.zeros(15) # zmienna z wartościami do kalibracji/wyzerowania studzienek przed pracą
+        self.__previousPositions = numpy.zeros(15) # zmienna referencyjna do ustawienia nowej pozycji
+
+    def Setter(self, _heights: numpy.ndarray): # setter wysokości
+        if(self.CheckData(_heights)=='OK', 200):
+            self.__heights = _heights
+            Functions.StartSettingHeights(self.__heights, self.__previousPositions)
+            self.__previousPositions = self.__heights
+        return self.CheckData(_heights)
+        
+    def CurrentPositionsSetter(self, _currentPositions: numpy.ndarray): # setter kalibracyjny
+        if(self.CheckData(_currentPositions)=='OK'):
+            self.__currentPositions = _currentPositions
+            Functions.StartCalibration(self.__currentPositions, self.__previousPositions)
+            self.__previousPositions = self.__currentPositions
+        return self.CheckData(_currentPositions)
+    
+    def Getter(self): # getter wysokości
+        return self.__heights
+    
+    def CheckData(self, _heights):
+        if(numpy.size(_heights)!=15):
+            return'ERROR: wrong size', 400
+        if(numpy.min(_heights)>=0 and numpy.max(_heights)<=60):
+            return 'OK', 200
+        else:
+            return 'ERROR: wrong value(s)', 400
+
+Heights = HeightsClass() # tworzenie obiektu Heights przechowującego informacje o wysokościach studzienek
+
+# endpointy
+
+@app.route('/')
+def default():
+    return "\nRaspberryPi nr 1 (1-15). Stan połączenia: OK "
+
+@app.route('/SetHeights', methods=['POST'])
+def SetHeights():
+    if(Functions.CheckMessage(request)==True):
+        receivedData = request.get_json()
+        return Heights.Setter(numpy.array(receivedData['Heights']))
+    else:
+        return Functions.CheckMessage(request)
+
+
+@app.route('/GetHeights', methods=['GET'])
+def GetHeights():
+    return {'Heights':Heights.Getter().tolist()}, 200
+
+@app.route('/Calibration', methods=['POST'])
+def Calibration():
+    if(Functions.CheckMessage(request)==True):
+        receivedData = request.get_json()
+        return Heights.CurrentPositionsSetter(numpy.array(receivedData['Heights']))
+    else:
+        return Functions.CheckMessage(request)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=55555)
